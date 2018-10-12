69bf8faa53a0044e79baa29859eab50459274c2a
Hari Kim <hari9497@Haris-MacBook-Pro.local>
2018-10-12 14:44:10 -0700

    first commit

---
+++ src/client/components/App.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+
+  }
+  // componentDidMount() {
+  //   fetch('/search')
+  //     .then((data) => {
+  //       return data.json();
+  //     })
+  //     .then()
+  // }
+
+  render() {
+    return (
+      <div>
+        <h1>My React App</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
