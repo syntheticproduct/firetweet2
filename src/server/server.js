094e70460389248cdaf44bc348af136f45374147
syntheticproduct <41520292+syntheticproduct@users.noreply.github.com>
2018-10-12 16:05:51 -0700

    sample tweet file & /tweets route

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = 3000;
 const app = express();
@@ -13,6 +15,12 @@ app.get('/', (req, res) => {
 app.get('/search', (req, res) => {
   res.status(200).send('FireTweet Search');
 });
+
+app.get('/tweets', (req, res) => {
+  let obj = JSON.parse(fs.readFileSync('./src/server/sampleTweets.JSON', 'utf8'));  
+  res.status(200).send(JSON.stringify(obj));
+});
+
 
 app.get('/search/:id', (req, res) => {
   res.status(200).send(`FireTweet Search for ${req.params.id}`);
