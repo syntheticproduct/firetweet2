4bffbf24290b889118c2d367275fe92e84b99159
Garron Ma <garronma@gmail.com>
2018-10-12 14:16:58 -0700

    get request reaches express succesfully

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const PORT = 3000;
 const app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.status(200).send('FireTweet Home');
 });
 
 app.get('/search', (req, res) => {
-  res.status(200).send('FireTweet Search');
+  console.log(req.query.search);
+  res.status(200).sendFile(path.join(`${__dirname}/client/search.html`));
 });
 
-app.get('/search/:id', (req, res) => {
+
+app.post('/search/:id', (req, res) => {
   res.status(200).send(`FireTweet Search for ${req.params.id}`);
 });
 
