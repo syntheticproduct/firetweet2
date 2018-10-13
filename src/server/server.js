dd6364af15b08ff29eef3a7d04bf3c0d2f7bf706
syntheticproduct <41520292+syntheticproduct@users.noreply.github.com>
2018-10-13 12:49:21 -0700

    cleanup twitter config

--- src/server/server.js
+++ src/server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const mongoose = require('mongoose');
-const path = require('path');
 
 const alertController = require('./alertController');
 
@@ -23,32 +22,6 @@ app.use(function (req, res, next) {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  res.status(200).send('FireTweet Home');
-});
-
-app.get('/alert', (req, res) => {
-  res.status(200).send('FireTweet Alert');
-});
-
-// app.post('/alert', (req, res) => {
-//   console.log(req.body);
-// });
-
-// app.get('/search', (req, res) => {
-//   console.log(req.query);
-//   res.status(200).sendFile(path.join(`${__dirname}/client/search.html`));
-// });
-
-// app.get('/tweets', (req, res) => {
-//   let obj = JSON.parse(fs.readFileSync('./src/server/sampleTweets.JSON', 'utf8'));
-//   res.status(200).send(JSON.stringify(obj));
-// });
-
-// app.get('/search/:id', (req, res) => {
-//   res.status(200).send(`FireTweet Search for ${req.params.id}`);
-// });
 
 // Begin routes for alerts
 
