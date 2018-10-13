4d08f7d53e8a4604485686203fd9886a46d75196
Garron Ma <garronma@gmail.com>
2018-10-13 14:58:17 -0700

    delete old code

--- src/server/server.js
+++ src/server/server.js
@@ -32,24 +32,6 @@ app.get('/alert', (req, res) => {
   res.status(200).send('FireTweet Alert');
 });
 
-// app.post('/alert', (req, res) => {
-//   console.log(req.body);
-// });
-
-// app.get('/search', (req, res) => {
-//   console.log(req.query);
-//   res.status(200).sendFile(path.join(`${__dirname}/client/search.html`));
-// });
-
-// app.get('/tweets', (req, res) => {
-//   let obj = JSON.parse(fs.readFileSync('./src/server/sampleTweets.JSON', 'utf8'));
-//   res.status(200).send(JSON.stringify(obj));
-// });
-
-// app.get('/search/:id', (req, res) => {
-//   res.status(200).send(`FireTweet Search for ${req.params.id}`);
-// });
-
 // Begin routes for alerts
 
 app.get('/alerts',
