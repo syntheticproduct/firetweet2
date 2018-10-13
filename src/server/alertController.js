f1e6db793a41b3d5e2451cc274c77abff53fbdd4
Hari Kim <hari9497@Haris-MacBook-Pro.local>
2018-10-12 22:13:06 -0700

    updated Controller

--- src/server/alertController.js
+++ src/server/alertController.js
@@ -7,6 +7,23 @@ alertController.getAlerts = (req, res, next) => {
     if (err) return res.status(500).json(err);
     res.locals.alerts = alerts;
     return next();
+  });
+}
+
+alertController.postAlert = (req, res, next) => {
+  const { query, email, phone } = req.body;
+  Alert.create({ query, email, phone }, (err, newAlert) => {
+    if (err) return res.status(500).json(err);
+    res.locals.newAlert = newAlert;
+    return next();
+  });
+}
+
+alertController.deleteAlert = (req, res, next) => {
+  console.log(req.body.id);
+  Alert.remove({ _id: req.body.id }, (err) => {
+    if (err) return res.status(500).json(err);
+    return next();
   })
 }
 
